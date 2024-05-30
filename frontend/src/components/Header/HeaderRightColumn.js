d1032fbec845aa58f17c47378581955dc45a3774
laps78 <sovtem@gmail.com>
2024-05-30 13:58:06 +0300

    добавил обработку статуса авторизации в компонент правой колонки хэдера страниц

--- frontend/src/components/Header/HeaderRightColumn.js
+++ frontend/src/components/Header/HeaderRightColumn.js
@@ -1,5 +1,11 @@
+import { useContext } from "react";
+import AppContext from "../../contexts/AppContext";
+import { Link } from "react-router-dom";
+
 export default function HeaderRightColumn() {
-  return (
+  const { user } = useContext(AppContext);
+
+  const unAuthorizedContent = (
     <div className="header_login_link">
       <a href="gui/login" className="App-link">
         Вход
@@ -7,4 +13,10 @@ export default function HeaderRightColumn() {
       | <a href="/gui/register">Регистрация</a>
     </div>
   );
+  const authorizedContent = <Link to="/api/auth/logout">Выход</Link>;
+
+  if (user) {
+    return <>{authorizedContent}</>;
+  }
+  return <>{unAuthorizedContent}</>;
 }
