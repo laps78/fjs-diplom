f4ec8117c6ddd1cc343b25f508b9cf8c085428db
laps78 <sovtem@gmail.com>
2024-06-03 18:02:54 +0300

    Удалено неиспользуемое объявление

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AppBrowserRouterArray from "./AppBrowserRouter";
-import { v4 as uuid } from "uuid";
 
 function App() {
   return (
